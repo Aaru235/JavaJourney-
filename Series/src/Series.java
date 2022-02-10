b5c23e44320fc9dac7af06d703d545c8b5948cec
AarushiVyass <69616360+Aaru235@users.noreply.github.com>
2022-02-11 01:46:07 +0530

    Update Series.java

--- Series/src/Series.java
+++ Series/src/Series.java
@@ -4,6 +4,7 @@ class Series
 	public static void main(String args[])
 	{
 		Scanner sc= new Scanner(System.in);
+		System.out.println("n=");
 		int n= sc.nextInt();
 		double s=0;
 		for(int i=1;i<=n;i++)
@@ -11,7 +12,12 @@ class Series
 			s= s + Math.pow(i, 2);
 		}
 		
-		System.out.println("sum="+s);
+		System.out.println("sum ="+s);
 		
 	}
 }
+
+//Basic series to get understanding
+//OUTPUT:
+//n= 3 (terms)
+//sum = 14.0 (1^2 + 2^2 +3^2)
